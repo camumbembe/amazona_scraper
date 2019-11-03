9a0abebd1f36df26f94ee60b86ffb97a121cc033
camumbembe <rafaellamgsouza@gmail.com>
2019-11-03 20:45:41 -0300

    form with semantic ui

--- amazona_app/src/components/SignUp.js
+++ amazona_app/src/components/SignUp.js
@@ -1,34 +1,43 @@
 import React from 'react';
 import './SignUp.css';
-// import {Label, Input, Field, Control} from 'react-bulma-components/lib/components/form';
 import TextField from '@material-ui/core/TextField';
 
+//props são informações passadas do componente pai para o componente filho;
 
 class SignUp extends React.Component{
 
-    state={name:'', email:'', idade:''}
+    state={email:'', usuario:''} //estados do componente
     
     onChange = (event) => {
-        console.log(event)
-        if (event.target.name=='name') {
-            this.setState({name:event.target.value})            
-        }
-        else if (event.target.name=='email') {
+        console.log(event.target.value)
+        if (event.target.name=='email') {
             this.setState({email:event.target.value})
         }
-        else if (event.target.name=='idade') {
-            this.setState({idade:event.target.value})
+        else if (event.target.name=='usuario') {
+            if (event.target.value[0]=='@') {
+                this.setState({usuario:event.target.value})                
+            }
+            else {
+                this.setState({usuario:'@' + event.target.value})
+            } 
+            // this.setState({usuario: event.target.value[0]=='@'? event.target.value : '@' + event.target.value})
         } 
+
     };
 
     render() {
         return (
             <div className='sign_up'>
                 <h1>Registro</h1>
-                <form noValidate autoComplete="off">
-                        <TextField onChange={this.onChange} name='name' value={this.state.name} label='Text input' />
-                        <TextField onChange={this.onChange} name='email' value={this.state.email} label='E-mail input' />
-                        <TextField onChange={this.onChange} name='idade' value={this.state.idade} label='Idade input' />
+                <form className="container">
+                        <div className="ui input " >
+                            <input onChange={this.onChange} type="text" name='email' placeholder="E-mail" value={this.state.email}/>
+                        </div>
+                        <div className="ui input " >
+                            <input onChange={this.onChange} type="text" name='usuario' placeholder="Usuário Telegram" value={this.state.usuario}/>
+                        </div>
+                        {/* <TextField className='sign_teste' onChange={this.onChange} name='email'  label='E-mail' />
+                        <TextField onChange={this.onChange} name='usuario'  label='Usuário Telegram' /> */}
                 </form>
             </div>
         )        
--- amazona_app/src/index.js
+++ amazona_app/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
-import 'typeface-roboto';
+import App from './components/App';  //import do component App(no file App)
+import 'semantic-ui-css/semantic.min.css';
 
-
-ReactDOM.render(<App />, document.querySelector('#root') );
+//O react dom vai renderizar o App no id root do DOM
+ReactDOM.render(<App />, document.querySelector('#root') ); 
 
